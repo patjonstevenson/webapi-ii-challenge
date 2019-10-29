4b52e3b3987abccabacd5a9626abce0f09154a74
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-29 12:03:05 -0700

    set up skeleton for posts-router

---
+++ posts/posts-router.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const posts = require("../data/db");
+
+// POST "/"
+router.post("/", (req, res) => {
+
+});
+
+// POST "/:id/comments"
+router.post("/:id/comments", (req, res) => {
+
+});
+
+// GET "/"
+router.get("/", (req, res) => {
+
+});
+
+// GET "/:id"
+router.get("/:id", (req, res) => {
+
+});
+
+// GET "/:id/comments"
+router.get("/:id/comments", (req, res) => {
+
+});
+
+// DELETE "/:id"
+router.delete("/:id", (req, res) => {
+
+});
+
+// PUT "/:id"
+router.put("/:id", (req, res) => {
+
+});
